40402bdfcf0b21f1bd989497f9f3c59811cdd813
Slatt77 <enerstory1@gmail.com>
2022-12-20 22:34:29 -0500

    About to proceed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Game from Pluto</h1>
+        <h1>Game from Pluto
+          Revamp
+        </h1>
     </div>
   );
 }
